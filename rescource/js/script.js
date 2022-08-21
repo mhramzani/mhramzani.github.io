ed1dc90c33d3d58508f6b25125a56720f81a602e
Mohammad Ramzani <mohammad.hr2000@gmail.com>
2022-08-21 18:57:37 +0430

    added night mode

--- rescource/js/script.js
+++ rescource/js/script.js
@@ -21,3 +21,24 @@ calcDate();
 //share 
 
 shareBtn.addEventListener('click', () => { navigator.share(shareData) });
+const topWave = get('.top-wave');
+const bottomWave = get('.bottom-wave')
+const footerWave = get('.top-footer')
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        topWave.setAttribute("style", "background-image: url(https://mhramzani.github.io/rescource/css/img/midWave-01-light.png);");
+        bottomWave.setAttribute("style", "background-image: url(https://mhramzani.github.io/rescource/css/img/bottomwave-light.png);");
+        footerWave.setAttribute("style", "background-image: url(https://mhramzani.github.io/rescource/css/img/footer-light.png);");
+
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        topWave.setAttribute("style", "background-image: url(https://mhramzani.github.io/rescource/css/img/midWave-01.png);");
+        bottomWave.setAttribute("style", "background-image: url(https://mhramzani.github.io/rescource/css/img/bottomwave.png);");
+        footerWave.setAttribute("style", "background-image: url(https://mhramzani.github.io/rescource/css/img/footer.png);");
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
